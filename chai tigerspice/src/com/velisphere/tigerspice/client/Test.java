fb246a5eec216b69894588c6c3d9542281d9ff95
thmeu <thorsten@thorsten-meudt.de>
2013-08-15 17:29:37 +0200

    Initial commit for tigerspice

---
+++ chai tigerspice/src/com/velisphere/tigerspice/client/Test.java
@@ -0,0 +1,13 @@
+package com.velisphere.tigerspice.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Test implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
