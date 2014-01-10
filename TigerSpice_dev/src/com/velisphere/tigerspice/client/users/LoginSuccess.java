0476198063830b71fdea46e6818ff8e6123999c8
Thorsten Meudt <thorsten@thorsten-meudt.de>
2014-01-10 15:25:11 +0100

    Improved login procedure and session handling

--- TigerSpice_dev/src/com/velisphere/tigerspice/client/users/LoginSuccess.java
+++ TigerSpice_dev/src/com/velisphere/tigerspice/client/users/LoginSuccess.java
@@ -21,37 +21,31 @@ package com.velisphere.tigerspice.client.users;
 
 import com.github.gwtbootstrap.client.ui.Bar;
 import com.github.gwtbootstrap.client.ui.Breadcrumbs;
-import com.github.gwtbootstrap.client.ui.Container;
-import com.github.gwtbootstrap.client.ui.Image;
 import com.github.gwtbootstrap.client.ui.NavLink;
 import com.github.gwtbootstrap.client.ui.PageHeader;
 import com.github.gwtbootstrap.client.ui.StackProgressBar;
-import com.github.gwtbootstrap.client.ui.TextBox;
+import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Position;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.velisphere.tigerspice.client.Login;
-import com.velisphere.tigerspice.client.LoginDialogBox;
 import com.velisphere.tigerspice.client.NavBar;
-import com.velisphere.tigerspice.client.endpointclasses.EPCList;
-import com.velisphere.tigerspice.client.endpoints.EndpointList;
-import com.velisphere.tigerspice.client.helper.Banderole;
-import com.velisphere.tigerspice.client.properties.PropertyList;
+import com.velisphere.tigerspice.client.helper.EventUtils;
+import com.velisphere.tigerspice.client.helper.SessionHelper;
+import com.velisphere.tigerspice.client.helper.SessionVerifiedEvent;
+import com.velisphere.tigerspice.client.helper.SessionVerifiedEventHandler;
 import com.velisphere.tigerspice.shared.UserData;
 
 
-public class LoginSuccess extends Composite {
+public class LoginSuccess extends Composite  {
 
 interface MyBinder extends UiBinder<Widget, LoginSuccess>{}
 	
@@ -74,90 +68,55 @@ interface MyBinder extends UiBinder<Widget, LoginSuccess>{}
 	
  	
 	public LoginSuccess() {
+	    
 		
-		 String sessionID = Cookies.getCookie("sid");
-	       System.out.println("Session ID: " +sessionID);
-	       if (sessionID == null)
-	       {
-	       	Login loginScreen = new Login();
-	   		loginScreen.onModuleLoad();
-	       } else
-	       {
-	           checkWithServerIfSessionIdIsStillLegal(sessionID);
-	       }
-	       
-		initWidget(uiBinder.createAndBindUi(this));	
-		bread0 = new NavLink();
-		bread0.setText("Home");
-		brdMain.add(bread0);
+	
+		System.out.println(SessionHelper.getCurrentUserName());
+		
+		initWidget(uiBinder.createAndBindUi(this));
+		loadContent();
+		
+	
 		
 
 		
-	}	
-
+	}
 	   
 	
 	public void loadContent(){
 
+		
+		
+		//initWidget(uiBinder.createAndBindUi(this));	
 		// set history for back button support
 		History.newItem("login_success");
-		// always reload the nav bar as it is context sensitive
-		rootPanelHeader = RootPanel.get("stockList");
-		rootPanelHeader.clear();
-		rootPanelHeader.getElement().getStyle().setPosition(Position.RELATIVE);
-		navBar = new NavBar();
-		rootPanelHeader.add(navBar);
 		
+		//
+		
+		
+		// set page header welcome back message
+    	pageHeader.setText("Welcome Back, " + SessionHelper.getCurrentUserName());
+    	pgbGreen.setPercent(30);
+    	
+    	pgbRed.setPercent(0);
+    	pgbYellow.setPercent(0);
+    	pgbGreen.setText("30/100 endpoints activated");
+    	
+		NavBar navBar = new NavBar();
+		navBar = (NavBar) RootPanel.get("stockList").getWidget(0);
+		navBar.activateForCurrentUser();
+		
+		bread0 = new NavLink();
+		bread0.setText("Home");
+		brdMain.add(bread0);
+
 		
 	}
 		
 
 	
 	
-	private void checkWithServerIfSessionIdIsStillLegal(String sessionID)
-	{
-	    LoginService.Util.getInstance().loginFromSessionServer(new AsyncCallback<UserData>()
-	    {
-	        @Override
-	        public void onFailure(Throwable caught)
-	        {
-	        	Login loginScreen = new Login();
-	    		loginScreen.onModuleLoad();
-	        }
-	 
-	        @Override
-	        public void onSuccess(UserData result)
-	        {
-	            if (result == null)
-	            {
-	            	Login loginScreen = new Login();
-	        		loginScreen.onModuleLoad();
-	            } else
-	            {
-	                if (result.getLoggedIn())
-	                {
-	                	// load all other content
-	                	loadContent();
-	            		// set page header welcome back message
-	                	pageHeader.setText("Welcome Back, " + result.userName);
-	                	pgbGreen.setPercent(30);
-	                	
-	                	pgbRed.setPercent(0);
-	                	pgbYellow.setPercent(0);
-	                	pgbGreen.setText("30/100 endpoints activated");
-	                	
-	                } else
-	                {
-	                	Login loginScreen = new Login();
-		        		loginScreen.onModuleLoad();
-	                }
-	            }
-	        }
-	 
-	    });
-	}
-
-		
+			
 	
 	
 	
