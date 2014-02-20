b355783b7cbc4539980fe0014fe3b153b7e016b2
Thorsten Meudt <thorsten@thorsten-meudt.de>
2014-02-20 20:37:50 +0100

    added dummy marketplace

---
+++ TigerSpice_dev/src/com/velisphere/tigerspice/client/marketplace/MarketPlace.java
@@ -0,0 +1,56 @@
+package com.velisphere.tigerspice.client.marketplace;
+
+import com.github.gwtbootstrap.client.ui.Breadcrumbs;
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.NavLink;
+import com.github.gwtbootstrap.client.ui.TextBox;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.velisphere.tigerspice.client.users.LoginSuccess;
+
+public class MarketPlace extends Composite {
+
+	@UiField
+	Breadcrumbs brdMain;
+	
+	NavLink bread0;
+	NavLink bread1;
+	NavLink bread2;
+	
+	private static MarketPlaceUiBinder uiBinder = GWT
+			.create(MarketPlaceUiBinder.class);
+
+	interface MarketPlaceUiBinder extends UiBinder<Widget, MarketPlace> {
+	}
+
+	public MarketPlace() {
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		bread0 = new NavLink();
+		bread0.setText("Home");
+		brdMain.add(bread0);
+		bread1 = new NavLink();
+		bread1.setText("Market Place");
+		brdMain.add(bread1);
+	
+		
+		
+		
+		bread0.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+
+				RootPanel.get("main").clear();
+				LoginSuccess loginSuccess = new LoginSuccess();
+				RootPanel.get("main").add(loginSuccess);
+
+			}
+		});
+	}
+
+}
