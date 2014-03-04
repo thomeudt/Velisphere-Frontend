ea10c117a605077df809660895d1c2c14c53c0c5
Thorsten Meudt <thorsten@thorsten-meudt.de>
2014-03-04 14:26:26 +0100

    Applied new copyright

--- TigerSpice_dev/src/com/velisphere/tigerspice/server/ServerParameters.java
+++ TigerSpice_dev/src/com/velisphere/tigerspice/server/ServerParameters.java
@@ -18,6 +18,6 @@
 package com.velisphere.tigerspice.server;
 
 public class ServerParameters {
-	public static String volt_ip = "16.1.1.149"; // for local db
+	public static String volt_ip = "16.1.1.110"; // for local db
 	//public static String volt_ip = "ec2-54-213-118-41.us-west-2.compute.amazonaws.com"; // for aws db
 }
