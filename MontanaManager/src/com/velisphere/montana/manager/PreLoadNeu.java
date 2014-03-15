069189e2e884d9ea3ae08175149f6b8d61c95348
Thorsten Meudt <thorsten@thorsten-meudt.de>
2014-03-15 11:34:34 +0100

    final before merge

--- MontanaManager/src/com/velisphere/montana/manager/PreLoadNeu.java
+++ MontanaManager/src/com/velisphere/montana/manager/PreLoadNeu.java
@@ -16,7 +16,7 @@ public class PreLoadNeu {
 	    	//public static String volt_ip = "ec2-54-200-17-56.us-west-2.compute.amazonaws.com"; // for aws db
 	        
 	        preLoader.createConnection("16.1.1.110");
-	        //preLoader.createConnection("ec2-54-186-8-25.us-west-2.compute.amazonaws.com");
+	        //preLoader.createConnection("ec2-54-186-85-248.us-west-2.compute.amazonaws.com");
 	        
 	        /*
 	         * Load the database.
--- MontanaManager/src/com/velisphere/montana/manager/PreLoadPart.java
+++ MontanaManager/src/com/velisphere/montana/manager/PreLoadPart.java
@@ -16,13 +16,13 @@ public class PreLoadPart {
 	    	// public static String volt_ip = "ec2-54-200-208-195.us-west-2.compute.amazonaws.com"; // for aws db
 	        
 	         preLoader.createConnection("16.1.1.110");
-	        //preLoader.createConnection("ec2-54-213-118-41.us-west-2.compute.amazonaws.com");
+	        //preLoader.createConnection("ec2-54-186-85-248.us-west-2.compute.amazonaws.com");
 	        
 	        /*
 	         * Load the database.
 	         */
 	        
-	        String userID = "a1a970d8-baa9-4fe2-bb77-9f5032854928";
+	        String userID = "9b7df343-801a-4ef6-a6ed-4586b80e8e1f";
 	        	        
 	        preLoader.callProcedure("ENDPOINT_USER_LINK.insert", "1007", "E1", userID);
 	        preLoader.callProcedure("ENDPOINT_USER_LINK.insert", "1006", "E2", userID);
