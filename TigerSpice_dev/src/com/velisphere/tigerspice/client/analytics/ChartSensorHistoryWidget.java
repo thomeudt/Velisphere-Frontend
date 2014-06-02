6663ca65022c24bdb962db6b857de33f1df3dd2c
Thorsten Meudt <thorsten@thorsten-meudt.de>
2014-06-02 22:37:26 +0200

    simple data table works in analytics

--- TigerSpice_dev/src/com/velisphere/tigerspice/client/analytics/ChartSensorHistoryWidget.java
+++ TigerSpice_dev/src/com/velisphere/tigerspice/client/analytics/ChartSensorHistoryWidget.java
@@ -1,9 +1,12 @@
 package com.velisphere.tigerspice.client.analytics;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.CellTable;
 import com.github.gwtbootstrap.client.ui.Column;
 import com.github.gwtbootstrap.client.ui.ListBox;
 import com.github.gwtbootstrap.client.ui.Paragraph;
@@ -13,11 +16,15 @@ import com.github.gwtbootstrap.client.ui.resources.ButtonSize;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
+import com.google.gwt.user.cellview.client.SimplePager;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.visualization.client.AbstractDataTable;
 import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
 import com.google.gwt.visualization.client.DataTable;
@@ -146,13 +153,17 @@ public class ChartSensorHistoryWidget extends Composite {
 		lbxPropertyFilter.addChangeHandler(new ChangeHandler() {
 			@Override
 			public void onChange(ChangeEvent event) {
-				
+
 				graphCol.add(new Paragraph("Loading chart..."));
 
 				Runnable onLoadCallback = new Runnable() {
 					public void run() {
 
-						populateChart(lbxEndpointFilter.getItemText(lbxEndpointFilter.getSelectedIndex()), lbxPropertyFilter.getItemText(lbxPropertyFilter.getSelectedIndex()));
+						populateChart(lbxEndpointFilter
+								.getItemText(lbxEndpointFilter
+										.getSelectedIndex()), lbxPropertyFilter
+								.getItemText(lbxPropertyFilter
+										.getSelectedIndex()));
 					}
 				};
 
@@ -249,19 +260,31 @@ public class ChartSensorHistoryWidget extends Composite {
 
 						}
 
-						populateChart(lbxEndpointFilter.getItemText(lbxEndpointFilter.getSelectedIndex()), lbxPropertyFilter.getItemText(lbxPropertyFilter.getSelectedIndex()));
+						populateChart(lbxEndpointFilter
+								.getItemText(lbxEndpointFilter
+										.getSelectedIndex()), lbxPropertyFilter
+								.getItemText(lbxPropertyFilter
+										.getSelectedIndex()));
 					}
 				});
 
 	}
 
 	private void populateChart(String endpointName, String propertyName) {
-		
+
 		graphCol.clear();
+
+		// container class for cell table
+
+		class SensorTrail {
+			private String timeStamp;
+			private String value;
+		}
 
 		final Options options = Options.create();
 
-		options.setTitle("Sensor Data Trail for Sensor '" + propertyName + "' on Endpoint '"+ endpointName+"'");
+		options.setTitle("Sensor Data Trail for Sensor '" + propertyName
+				+ "' on Endpoint '" + endpointName + "'");
 		// options.setWidth(vp.getOffsetWidth());
 		options.setHeight((int) (RootPanel.get().getOffsetHeight() * 0.3));
 		options.setFontName("Source Sans Pro");
@@ -270,8 +293,10 @@ public class ChartSensorHistoryWidget extends Composite {
 		final AnalyticsServiceAsync analyticsService = GWT
 				.create(AnalyticsService.class);
 
-		System.out.println(lbxEndpointFilter.getValue() + " / " + lbxPropertyFilter.getValue());
-		analyticsService.getEndpointLog(lbxEndpointFilter.getValue(), lbxPropertyFilter.getValue(),
+		System.out.println(lbxEndpointFilter.getValue() + " / "
+				+ lbxPropertyFilter.getValue());
+		analyticsService.getEndpointLog(lbxEndpointFilter.getValue(),
+				lbxPropertyFilter.getValue(),
 				new AsyncCallback<LinkedList<EndpointLogData>>() {
 					@Override
 					public void onFailure(Throwable caught) {
@@ -283,6 +308,43 @@ public class ChartSensorHistoryWidget extends Composite {
 					public void onSuccess(LinkedList<EndpointLogData> result) {
 						// TODO Auto-generated method stub
 
+						// prep the datatable
+
+						CellTable<SensorTrail> table = new CellTable<SensorTrail>();
+
+						TextColumn<SensorTrail> timeStampColumn = new TextColumn<SensorTrail>() {
+							@Override
+							public String getValue(SensorTrail entry) {
+								return entry.timeStamp;
+							}
+						};
+						
+						timeStampColumn.setSortable(true);
+
+						TextColumn<SensorTrail> valueColumn = new TextColumn<SensorTrail>() {
+							@Override
+							public String getValue(SensorTrail entry) {
+								return entry.value;
+							}
+						};
+						
+						valueColumn.setSortable(true);
+
+						// Add the columns.
+						table.addColumn(timeStampColumn, "Time Stamp");
+						table.addColumn(valueColumn, "Value");
+						table.setRowCount(result.size(), true);
+						table.setVisibleRange(0, 10);
+
+						// Create a data provider.
+						ListDataProvider<SensorTrail> dataProvider = new ListDataProvider<SensorTrail>();
+
+						// Connect the table to the data provider.
+						dataProvider.addDataDisplay(table);
+
+						// create the datatable for chart and celltable for
+						// table
+
 						DataTable data = DataTable.create();
 						data.addColumn(ColumnType.STRING, "Time");
 						data.addColumn(ColumnType.NUMBER, "Value");
@@ -291,6 +353,7 @@ public class ChartSensorHistoryWidget extends Composite {
 
 						int i = 1;
 						Iterator<EndpointLogData> it = result.iterator();
+						List<SensorTrail> list = dataProvider.getList();
 
 						while (it.hasNext()) {
 
@@ -298,6 +361,16 @@ public class ChartSensorHistoryWidget extends Composite {
 							data.setValue(i, 0, logItem.getTimeStamp());
 							data.setValue(i, 1,
 									Integer.parseInt(logItem.getValue()));
+
+							// Add the data to the data provider, which
+							// automatically pushes it to the
+							// widget.
+							
+							SensorTrail trailItem = new SensorTrail();
+							trailItem.timeStamp = logItem.getTimeStamp();
+							trailItem.value = logItem.getValue();
+							list.add(trailItem);
+
 							i = i + 1;
 
 						}
@@ -305,6 +378,40 @@ public class ChartSensorHistoryWidget extends Composite {
 						// Create a line chart visualization.
 						LineChart lines = new LineChart(data, options);
 						graphCol.add(lines);
+
+						graphCol.add(new Paragraph("Data Table:"));
+						
+						graphCol.add(table);
+						
+						SimplePager test = new SimplePager();
+						test.setDisplay(table);						
+						
+						graphCol.add(test);
+						graphCol.add(new Paragraph(" "));
+						
+						/**
+						 * this does not work yet
+						 */
+						
+						// Add a ColumnSortEvent.ListHandler to connect sorting to the
+					    // java.util.List.
+					    ListHandler<SensorTrail> columnSortHandler = new ListHandler<SensorTrail>(list);
+					    columnSortHandler.setComparator(timeStampColumn,
+					        new Comparator<SensorTrail>() {
+					          public int compare(SensorTrail o1, SensorTrail o2) {
+					            if (o1 == o2) {
+					              return 0;
+					            }
+
+					            // Compare the timestamp columns.
+					            if (o1 != null) {
+					              return (o2 != null) ? o1.timeStamp.compareTo(o2.timeStamp) : 1;
+					            }
+					            return -1;
+					          }
+					        });
+					    table.addColumnSortHandler(columnSortHandler);
+
 
 					}
 				});
