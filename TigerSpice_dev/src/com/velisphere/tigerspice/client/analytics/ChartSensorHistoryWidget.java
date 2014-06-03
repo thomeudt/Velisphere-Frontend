09539c76153afcfaea7eda68a4c8adfdb84e1062
Thorsten Meudt <thorsten@thorsten-meudt.de>
2014-06-03 21:38:04 +0200

    date range filter implemented in analytics

--- TigerSpice_dev/src/com/velisphere/tigerspice/client/analytics/ChartSensorHistoryWidget.java
+++ TigerSpice_dev/src/com/velisphere/tigerspice/client/analytics/ChartSensorHistoryWidget.java
@@ -1,11 +1,14 @@
 package com.velisphere.tigerspice.client.analytics;
 
+import java.sql.Timestamp;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.apache.james.mime4j.field.datetime.DateTime;
+
 import com.github.gwtbootstrap.client.ui.Button;
 import com.github.gwtbootstrap.client.ui.CellTable;
 import com.github.gwtbootstrap.client.ui.Column;
@@ -183,15 +186,21 @@ public class ChartSensorHistoryWidget extends Composite {
 		dtbEnd.setHighlightToday(true);
 		dtbEnd.setBaseIcon(IconType.CALENDAR);
 		dtbEnd.setAutoClose(true);
+		
 		filterCol10.add(dtbEnd);
 		filterRow10.add(filterCol10);
 
 		Row filterRow11 = new Row();
 		Column filterCol11 = new Column(2);
-		Button btnDownload = new Button("Download");
-		btnDownload.setSize(ButtonSize.SMALL);
-		btnDownload.setType(ButtonType.PRIMARY);
-		filterCol11.add(btnDownload);
+		Button btnDownloadChart = new Button("Download Chart");
+		btnDownloadChart.setSize(ButtonSize.SMALL);
+		btnDownloadChart.setType(ButtonType.PRIMARY);
+		filterCol11.add(btnDownloadChart);
+		Button btnDownloadTable = new Button("Download Data");
+		btnDownloadTable.setSize(ButtonSize.SMALL);
+		btnDownloadTable.setType(ButtonType.PRIMARY);
+		filterCol11.add(btnDownloadTable);
+
 
 		filterRow11.add(filterCol11);
 		
@@ -262,8 +271,15 @@ public class ChartSensorHistoryWidget extends Composite {
 			public void onValueChange(ValueChangeEvent event) {
 
 				
-				lbxPropertyFilter.fireEvent(new ChangeEvent() {});
-
+				Timestamp tsStart = new Timestamp(dtbStart.getValue().getTime());
+				Timestamp tsEnd = new Timestamp(dtbEnd.getValue().getTime());
+				
+				populateChartForRange(lbxEndpointFilter
+						.getItemText(lbxEndpointFilter
+								.getSelectedIndex()), lbxPropertyFilter
+						.getItemText(lbxPropertyFilter
+								.getSelectedIndex()), tsStart, tsEnd);
+			
 				
 			}
 		});
@@ -272,10 +288,15 @@ public class ChartSensorHistoryWidget extends Composite {
 			@Override
 			public void onValueChange(ValueChangeEvent event) {
 
+				Timestamp tsStart = new Timestamp(dtbStart.getValue().getTime());
+				Timestamp tsEnd = new Timestamp(dtbEnd.getValue().getTime());
 				
-				lbxPropertyFilter.fireEvent(new ChangeEvent() {});
-
-				
+				populateChartForRange(lbxEndpointFilter
+						.getItemText(lbxEndpointFilter
+								.getSelectedIndex()), lbxPropertyFilter
+						.getItemText(lbxPropertyFilter
+								.getSelectedIndex()), tsStart, tsEnd);
+							
 			}
 		});
 
@@ -586,6 +607,220 @@ public class ChartSensorHistoryWidget extends Composite {
 	}
 
 
+	private void populateChartForRange(String endpointName, String propertyName, final Timestamp startDate, final Timestamp endDate) {
+
+		
+
+		// container class for cell table
+
+		class SensorTrail {
+			private String timeStamp;
+			private Double value;
+			private Integer itemNumber;
+		}
+
+		final Options options = Options.create();
+
+		options.setTitle("Sensor Data Trail for Sensor '" + propertyName
+				+ "' on Endpoint '" + endpointName + "'");
+		// options.setWidth(vp.getOffsetWidth());
+		options.setHeight((int) (RootPanel.get().getOffsetHeight() * 0.3));
+		options.setFontName("Source Sans Pro");
+		options.setLineWidth(2);
+		
+
+		final AnalyticsServiceAsync analyticsService = GWT
+				.create(AnalyticsService.class);
+
+		System.out.println(lbxEndpointFilter.getValue() + " / "
+				+ lbxPropertyFilter.getValue());
+		
+		analyticsService.getEndpointLog(lbxEndpointFilter.getValue(),
+				lbxPropertyFilter.getValue(),
+				new AsyncCallback<LinkedList<EndpointLogData>>() {
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onSuccess(LinkedList<EndpointLogData> result) {
+						// TODO Auto-generated method stub
+
+						// prep the datatable
+
+						final CellTable<SensorTrail> table = new CellTable<SensorTrail>(10, GWT.<CellTable.SelectableResources>create(CellTable.SelectableResources.class));
+
+						TextColumn<SensorTrail> timeStampColumn = new TextColumn<SensorTrail>() {
+							@Override
+							public String getValue(SensorTrail entry) {
+								return entry.timeStamp;
+							}
+						};
+						
+						timeStampColumn.setSortable(true);
+
+												
+						com.google.gwt.user.cellview.client.Column<SensorTrail, Number> valueColumn = 
+								new com.google.gwt.user.cellview.client.Column<SensorTrail, Number>(new NumberCell(NumberFormat.getFormat("#,##0.00;-#,##0.00"))) {
+							@Override
+							public Number getValue(SensorTrail entry) {
+								return entry.value;
+							}
+						};
+						
+						valueColumn.setSortable(true);
+
+						// Add the columns.
+						table.addColumn(timeStampColumn, "Time Stamp");
+						table.addColumn(valueColumn, lbxPropertyFilter.getItemText(lbxPropertyFilter.getSelectedIndex()));
+						table.setRowCount(result.size(), true);
+						//table.setVisibleRange(0, 10);
+
+						// Create a data provider.
+						ListDataProvider<SensorTrail> dataProvider = new ListDataProvider<SensorTrail>();
+
+						// Connect the table to the data provider.
+						dataProvider.addDataDisplay(table);
+					
+
+						// create the datatable for chart and celltable for
+						// table
+
+						final DataTable data = DataTable.create();
+						data.addColumn(ColumnType.STRING, "Time");
+						data.addColumn(ColumnType.NUMBER, lbxPropertyFilter.getItemText(lbxPropertyFilter.getSelectedIndex()));
+
+						data.addRows(result.size() + 1);
+
+						int i = 1;
+						Iterator<EndpointLogData> it = result.iterator();
+						List<SensorTrail> list = dataProvider.getList();
+
+						while (it.hasNext()) {
+
+							
+							EndpointLogData logItem = it.next();
+							
+							if(Timestamp.valueOf(logItem.getTimeStamp()).after(startDate) && Timestamp.valueOf(logItem.getTimeStamp()).before(endDate))
+							{
+							
+							data.setValue(i, 0, logItem.getTimeStamp());
+							data.setValue(i, 1,	logItem.getValue());
+						
+
+							// Add the data to the data provider, which
+							// automatically pushes it to the
+							// widget.
+							
+							SensorTrail trailItem = new SensorTrail();
+							trailItem.timeStamp = logItem.getTimeStamp();
+							trailItem.value = logItem.getValue();
+							trailItem.itemNumber = i;
+							list.add(trailItem);
+							}
+
+							i = i + 1;
+
+						}
+
+						// Create a line chart visualization.
+						final LineChart lines = new LineChart(data, options);
+						graphCol.clear();
+						
+						graphCol.add(lines);
+
+
+						graphCol.add(new Paragraph("Data Table:"));
+						
+						
+											
+						
+						// Add a ColumnSortEvent.ListHandler to connect sorting to the
+					    // java.util.List.
+					    ListHandler<SensorTrail> columnSortHandler = new ListHandler<SensorTrail>(list);
+					    // Sort by TimeStamp
+					    columnSortHandler.setComparator(timeStampColumn,
+					        new Comparator<SensorTrail>() {
+					          public int compare(SensorTrail o1, SensorTrail o2) {
+					            if (o1 == o2) {
+					              return 0;
+					            }
+
+					            // Compare the timestamp columns.
+					            if (o1 != null) {
+					              return (o2 != null) ? o1.timeStamp.compareTo(o2.timeStamp) : 1;
+					            }
+					            return -1;
+					          }
+					        });
+					    
+					    // Sort by Value
+					    
+					    columnSortHandler.setComparator(valueColumn,
+						        new Comparator<SensorTrail>() {
+						          public int compare(SensorTrail o1, SensorTrail o2) {
+						            if (o1 == o2) {
+						              return 0;
+						            }
+
+						            // Compare the timestamp columns.
+						            if (o1 != null) {
+						              return (o2 != null) ? o1.value.compareTo(o2.value) : 1;
+						            }
+						            return -1;
+						          }
+						        });
+
+					    table.addColumnSortHandler(columnSortHandler);
+					    
+					    
+					    
+					    table.setStriped(false);
+					    table.setHover(true);
+					    
+					    table.getColumnSortList().push(timeStampColumn);
+					    table.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+					    
+					    
+					    // add selection model
+					    
+					    final SingleSelectionModel<SensorTrail> selectionModel = new SingleSelectionModel<SensorTrail>();
+					    table.setSelectionModel(selectionModel);
+					    selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+					      public void onSelectionChange(SelectionChangeEvent event) {
+					    	  SensorTrail selected = selectionModel.getSelectedObject();
+					    	  
+					        if (selected != null) {
+					          //Window.alert("You selected: " + selected.value);
+					        	
+					        	
+					        	JsArray arr = JavaScriptObject.createArray().cast();
+								arr.push(newJsEntry(selected.itemNumber.toString(), "1")); 
+								lines.setSelections(arr);
+					         
+					          
+					        }
+					      }
+					    });
+					    
+						
+						
+						graphCol.add(table);
+						
+						SimplePager test = new SimplePager();
+						test.setDisplay(table);						
+						
+						graphCol.add(test);
+						graphCol.add(new Paragraph(" "));
+						
+					}
+				});
+	}
+
+	
+	
 	private final native JavaScriptObject newJsEntry(String rowNo, 
             String colNo)/*-{
 	return {row: rowNo, col: colNo};
