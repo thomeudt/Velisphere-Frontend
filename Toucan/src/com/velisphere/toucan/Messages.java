e23689bdd0ceb91b2e36b3814282d65779b9f203
Thorsten Meudt <thorsten@thorsten-meudt.de>
2014-06-28 22:09:38 +0200

    added basic PUT to web service

--- Toucan/src/com/velisphere/toucan/Messages.java
+++ Toucan/src/com/velisphere/toucan/Messages.java
@@ -1,10 +1,12 @@
 package com.velisphere.toucan;
- 
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,172 +21,147 @@ import com.rabbitmq.client.ConsumerCancelledException;
 import com.rabbitmq.client.GetResponse;
 import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.ShutdownSignalException;
- 
+
+
 @Path("/messages")
-public class Messages  {
- 
-	/**
+public class Messages {
+
 	@GET
-	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
- 
-		String output = "Jersey says : " + msg;
- 
-		return Response.status(200).entity(output).build();
- 
-	} 
-	**/
-	
-	@GET
-	@Path("/xml/{param}")
+	@Path("/get/xml/{param}")
 	@Produces({ MediaType.APPLICATION_XML })
 	public Todo getXML(@PathParam("param") String endpointID) {
-	    Todo todo = new Todo();
-	    todo.setSummary("VeliSphere Web Service Version 0.1");
-	    //todo.setDescription("First Sphere");
-	    
-	    //AMQP handling from here
-	    
-	    ConfigHandler conf = new ConfigHandler();
-	    conf.loadParamChangesAsXML();
-	    
-	    String QUEUE_NAME = endpointID;
+		Todo todo = new Todo();
+		todo.setSummary("VeliSphere Web Service Version 0.1");
+		// todo.setDescription("First Sphere");
+
+		// AMQP handling from here
+
+		ConfigHandler conf = new ConfigHandler();
+		conf.loadParamChangesAsXML();
+
+		String QUEUE_NAME = endpointID;
 
 		try {
-	    ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost(ServerParameters.bunny_ip);
-	    Connection connection;
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost(ServerParameters.bunny_ip);
+			Connection connection;
 			connection = factory.newConnection();
-		
-	    Channel channel = connection.createChannel();
 
-	    //channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-		
-	    GetResponse response = channel.basicGet(QUEUE_NAME, true);
-	    
-	    LinkedList<String> test = new LinkedList<String>();
-	    
-	    while (response != null){
-	        AMQP.BasicProperties props = response.getProps();
-	        byte[] body = response.getBody();
-	        String message = new String(body);
-	        System.out.println("RECEIVED: " + message);
-	        test.add(message);
-	        test.add(MessagePack.extractProperty(message, "PR9"));
-	        long deliveryTag = response.getEnvelope().getDeliveryTag();
-	        response = channel.basicGet(QUEUE_NAME, true);
-	    }
-	    
-	    todo.setDescription(test);
-	    
-		}
-	     
-	    
-	    
-	    /**
-	    QueueingConsumer consumer = new QueueingConsumer(channel);
-	    channel.basicConsume(QUEUE_NAME, true, consumer);
-	    
-	    
-	    RabbitAdmin   getQueueProperties(queue.getName());
-        messageCount = Integer.parseInt(props.get("QUEUE_MESSAGE_COUNT").toString());
-        System.out.println(queue.getName() + " has " + messageCount + " messages");
-		
-	   while (!Thread.currentThread().isInterrupted()){
-	    QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-	    String message = new String(delivery.getBody());
-	    System.out.println(" [x] Received '" + message + "'");
-	  
-	   
-	   }
-	   
-	   
-	   }catch(InterruptedException e){
-	       Thread.currentThread().interrupt();
-	       
-	   }
-	   **/
-		 catch (IOException | ShutdownSignalException | ConsumerCancelledException e) {
+			Channel channel = connection.createChannel();
+
+			// channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			System.out
+					.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+			GetResponse response = channel.basicGet(QUEUE_NAME, true);
+
+			LinkedList<String> test = new LinkedList<String>();
+
+			while (response != null) {
+				AMQP.BasicProperties props = response.getProps();
+				byte[] body = response.getBody();
+				String message = new String(body);
+				System.out.println("RECEIVED: " + message);
+				test.add(message);
+				test.add(MessagePack.extractProperty(message, "PR9"));
+				long deliveryTag = response.getEnvelope().getDeliveryTag();
+				response = channel.basicGet(QUEUE_NAME, true);
+			}
+
+			todo.setDescription(test);
+
+		} catch (IOException | ShutdownSignalException
+				| ConsumerCancelledException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
- 	 return todo;
-	  }
+		return todo;
+	}
 
-	
 	@GET
-	@Path("/json/{param}")
+	@Path("/get/json/{param}")
 	@Produces({ MediaType.TEXT_PLAIN })
 	public String getJSON(@PathParam("param") String endpointID) {
-	    Todo todo = new Todo();
-	    todo.setSummary("VeliSphere Web Service Version 0.1");
-	    //todo.setDescription("First Sphere");
-	    
-	    //AMQP handling from here
-	    
-	    ConfigHandler conf = new ConfigHandler();
-	    conf.loadParamChangesAsXML();
-	    
-	    String QUEUE_NAME = endpointID;
+		Todo todo = new Todo();
+		todo.setSummary("VeliSphere Web Service Version 0.1");
+		// todo.setDescription("First Sphere");
 
-	    String test = new String();
-	    
+		// AMQP handling from here
+
+		ConfigHandler conf = new ConfigHandler();
+		conf.loadParamChangesAsXML();
+
+		String QUEUE_NAME = endpointID;
+
+		String test = new String();
+
 		try {
-	    ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost(ServerParameters.bunny_ip);
-	    Connection connection;
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost(ServerParameters.bunny_ip);
+			Connection connection;
 			connection = factory.newConnection();
-		
-	    Channel channel = connection.createChannel();
 
-	    //channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-		
-	    GetResponse response = channel.basicGet(QUEUE_NAME, true);
-	    
-	    
-	    
-	    while (response != null){
-	        AMQP.BasicProperties props = response.getProps();
-	        byte[] body = response.getBody();
-	        String message = new String(body);
-	        System.out.println("RECEIVED: " + message);
-	        test = test+","+message;
-	        //test.add(MessagePack.extractProperty(message, "PR9"));
-	        long deliveryTag = response.getEnvelope().getDeliveryTag();
-	        response = channel.basicGet(QUEUE_NAME, true);
-	    }
-	    
-	    
-	    if (test.length() > 0) test = "[" + test.substring(1) +"]";
-	    //todo.setDescription(test);
-	    
+			Channel channel = connection.createChannel();
+
+			// channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			System.out
+					.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+			GetResponse response = channel.basicGet(QUEUE_NAME, true);
+
+			while (response != null) {
+				AMQP.BasicProperties props = response.getProps();
+				byte[] body = response.getBody();
+				String message = new String(body);
+				System.out.println("RECEIVED: " + message);
+				test = test + "," + message;
+				// test.add(MessagePack.extractProperty(message, "PR9"));
+				long deliveryTag = response.getEnvelope().getDeliveryTag();
+				response = channel.basicGet(QUEUE_NAME, true);
+			}
+
+			if (test.length() > 0)
+				test = "[" + test.substring(1) + "]";
+			// todo.setDescription(test);
+
 		}
-	     
-	    
-	    		 catch (IOException | ShutdownSignalException | ConsumerCancelledException e) {
+
+		catch (IOException | ShutdownSignalException
+				| ConsumerCancelledException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
- 	 return test;
-	  }
+		return test;
+	}
 
-	
-	
-	 // This can be used to test the integration with the browser
-	
+	// This can be used to test the integration with the browser
+
 	@GET
 	@Path("/text")
-	  @Produces({ MediaType.TEXT_XML })
-	  public Todo getHTML() {
-	    Todo todo = new Todo();
-	    todo.setSummary("VeliSphere Web Service Version 0.1");
-	    //todo.setDescription("First Sphere");
-	    return todo;
-	  }
-	 
+	@Produces({ MediaType.TEXT_XML })
+	public Todo getHTML() {
+		Todo todo = new Todo();
+		todo.setSummary("VeliSphere Web Service Version 0.1");
+		// todo.setDescription("First Sphere");
+		return todo;
+	}
+	
+	
+	@PUT
+	@Path( "/put/text/{user}" )
+	@Consumes( MediaType.TEXT_PLAIN )
+	public Response postPlainTextMessage( @PathParam( "content" ) String user, String message ) throws Exception {
 
-	 
- 
+	
+	System.out.printf( "%s sendet ‘%s’%n", user, message );
+	HashMap<String,String>outboundMessageMap = new HashMap<String, String>();
+	outboundMessageMap.put("A", message);
+	String targetEPID = new String("EE");
+
+	Send.sendHashTable(outboundMessageMap, targetEPID, "test");
+	return Response.noContent().build();
+
+	}
+	
+
 }
---
+++ Toucan/src/com/velisphere/toucan/Send.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * CONFIDENTIAL INFORMATION
+ *  __________________
+ *  
+ *   Copyright (C) 2013 Thorsten Meudt 
+ *   All Rights Reserved.
+ *  
+ *  NOTICE:  All information contained herein is, and remains
+ *  the property of Thorsten Meudt and its suppliers,
+ *  if any.  The intellectual and technical concepts contained
+ *  herein are proprietary to Thorsten Meudt
+ *  and its suppliers and may be covered by Patents,
+ *  patents in process, and are protected by trade secret or copyright law.
+ *  Dissemination of this information or reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from Thorsten Meudt.
+ ******************************************************************************/
+package com.velisphere.toucan;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.AMQP.BasicProperties;
+
+public class Send {
+
+	String message = new String();
+	String queue_name = new String();
+
+	Send(String m, String q) {
+		message = m;
+		queue_name = q;
+	}
+
+	
+	
+	
+	public static void sendHashTable(HashMap<String, String> message,
+			String targetQueueName, String senderQueueName) throws Exception {
+
+		ConfigHandler conf = new ConfigHandler();
+		conf.loadParamChangesAsXML();
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(ServerParameters.bunny_ip);
+		Connection connection;
+		connection = factory.newConnection();
+
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare("EC", false, false, false, null);
+
+		ObjectMapper mapper = new ObjectMapper();
+		StringWriter writer = new StringWriter();
+		HashMap<String, String> messageMap = new HashMap<String, String>();
+		messageMap.put("SECTOK", null);
+		messageMap.put("TIMESTAMP", null);
+		messageMap.put("TYPE", "REG");
+		messageMap.put("EPID", senderQueueName);
+		messageMap.putAll(message);
+
+		
+		mapper.writeValue(writer, messageMap);
+		
+		//System.out.println("Target: " + targetQueueName);
+
+		channel.basicPublish("", targetQueueName, null,
+				writer.toString().getBytes());
+
+		// System.out.println(" [x] Sent '" + writer.toString() + "' from " + senderQueueName + " to " + targetQueueName);
+
+		channel.close();
+		
+	}
+	
+
+
+}
