36ece1be4478ab24b30d008a1b9e4baf6d6c35fe
Thorsten Meudt <thorsten@thorsten-meudt.de>
2014-07-01 21:35:10 +0200

    Toucan provisioning minor mods

--- Toucan/src/com/velisphere/toucan/Provisioning.java
+++ Toucan/src/com/velisphere/toucan/Provisioning.java
@@ -13,8 +13,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 
 
 import org.voltdb.client.NoConnectionsException;
@@ -89,8 +91,8 @@ public class Provisioning {
 	
 	
 	
-	
-	return Response.noContent().build();
+	return Response.ok(uEPID).build();
+	//return Response.noContent().build();
 
 	}
 	
