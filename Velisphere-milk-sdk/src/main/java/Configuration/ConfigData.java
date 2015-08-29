6f514765bcd074fc6394578213672daa68e94a62
thorsten <thorsten@thorsten-meudt.de>
2015-08-29 14:08:48 +0200

    Initial add of MILK SDK

---
+++ Velisphere-milk-sdk/src/main/java/Configuration/ConfigData.java
@@ -0,0 +1,6 @@
+package Configuration;
+
+public class ConfigData {
+	public static String secret = "";
+	public static String epid = "";
+}
---
+++ Velisphere-milk-sdk/src/main/java/Configuration/ConfigFileAccess.java
@@ -0,0 +1,59 @@
+package Configuration;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+
+public class ConfigFileAccess {
+	
+	public void saveParamChangesAsXML(String secret, String endpointID) {
+	    try {
+	        Properties props = new Properties();
+	        props.setProperty("Secret Key", secret);
+	        props.setProperty("Endpoint ID", endpointID);
+	        
+	        
+	        File f = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "velisphere_config.xml");
+	        OutputStream out = new FileOutputStream( f );
+	        props.storeToXML(out, "This file contains Velisphere authentication information. Do not overwrite!");
+	    }
+	    catch (Exception e ) {
+	        e.printStackTrace();
+	    }
+	}
+
+	
+	/**
+	 * Load the preferences
+	 * @return 
+	 */
+	
+	public static void loadParamChangesAsXML(){
+	
+		System.out.println(" [IN] Loading config from " + System.getProperty("user.dir") + System.getProperty("file.separator") + "velisphere_config.xml");
+		
+	Properties props = new Properties();
+    InputStream is = null;
+ 
+   
+    try {
+        File f = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "velisphere_config.xml");
+        is = new FileInputStream( f );
+        props.loadFromXML( is );
+    }
+    catch ( Exception e ) { is = null; }
+ 
+      
+    
+    ConfigData.secret = props.getProperty("Secret Key");
+    ConfigData.epid = props.getProperty("Endpoint ID");
+    
+	}
+
+
+}
