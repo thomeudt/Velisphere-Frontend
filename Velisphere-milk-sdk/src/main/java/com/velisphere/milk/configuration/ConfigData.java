8e94d7d21e7fb195f863913dc8e41e92c770b42e
thorsten <thorsten@thorsten-meudt.de>
2015-11-29 22:12:50 +0100

    Updated FS to new SDK

--- Velisphere-milk-sdk/src/main/java/com/velisphere/milk/configuration/ConfigData.java
+++
@@ -1,8 +0,0 @@
-package com.velisphere.milk.configuration;
-
-public class ConfigData {
-	public static String secret = "";
-	public static String epid = "";
-	public static boolean provisioned = false;
-	public static String epcid = "";
-}
